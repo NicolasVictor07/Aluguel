39fa7a95b0e27220be6ba193f40b8f30badfc417
Nicolasvictor07 <Nicolasvictor0402@gmail.com>
2026-02-02 11:37:35 -0300

    views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Usuario, Imovel, Contrato, Pagamento
+from .serializers import UsuarioSerializer, ImovelSerializer, ContratoSerializer, PagamentoSerializer
 
-# Create your views here.
+class UsuarioListCreateAPIView(ListCreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
